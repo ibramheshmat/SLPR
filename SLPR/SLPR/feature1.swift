99faacca20d22f415683e9fca72803e020cf2418
Ibram Heshmat (SDD) <ibram.heshmat@sdd.shj.ae>
2026-05-21 15:44:17 +0400

    start add params

--- SLPR/SLPR/feature1.swift
+++ SLPR/SLPR/feature1.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class feature1: UIViewController {
 
-    
+    let skfhjakjghadjkghdajkghjdskjgjdsklgjkdsg= "sfjahfkjahfjk"
     override func viewDidLoad() {
         super.viewDidLoad()
         hello()
