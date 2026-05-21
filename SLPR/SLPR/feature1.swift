c4519c155cb3c8bb6b7020fdac43a6f73b0a9681
Ibram Heshmat (SDD) <ibram.heshmat@sdd.shj.ae>
2026-05-21 15:51:30 +0400

    fix PR

--- SLPR/SLPR/feature1.swift
+++ SLPR/SLPR/feature1.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class feature1: UIViewController {
 
-    let skfhjakjghadjkghdajkghjdskjgjdsklgjkdsg= "sfjahfkjahfjk"
+    let sk = "sfjahfkjahfjk"
     override func viewDidLoad() {
         super.viewDidLoad()
         hello()
