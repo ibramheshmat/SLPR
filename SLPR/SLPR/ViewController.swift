922e6e220013c8183394dd01b5a641b46f98cf4c
Ibram Heshmat (SDD) <ibram.heshmat@sdd.shj.ae>
2026-06-01 11:13:18 +0400

    try some thing wrong

--- SLPR/SLPR/ViewController.swift
+++ SLPR/SLPR/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var dsljghsklgfklgklfgjfkdlgfdklgjfklgjdasiyftaiuyftyasfiuysfiuyasfiuaysfisdlghasklghjadglf = "test"
     override func viewDidLoad() {
         super.viewDidLoad()
         hello()
